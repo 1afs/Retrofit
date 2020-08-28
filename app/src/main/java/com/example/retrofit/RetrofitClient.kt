a44792a7eaf3bd5fa95be91f9719c061d6c85e4c
1afs <1afs.andre@gmail.com>
2020-08-28 15:45:36 -0300

    Initial commit

---
+++ app/src/main/java/com/example/retrofit/RetrofitClient.kt
@@ -0,0 +1,37 @@
+package com.example.retrofit
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+//Singleton
+class RetrofitClient private constructor(){
+
+ companion object{
+
+     private lateinit var retrofit: Retrofit
+     private val baseUrl = "http://jsonplaceholder.typicode.com/"
+
+
+
+     private fun getRetrofitInstance(): Retrofit{
+
+         val httpClient = OkHttpClient.Builder()
+         if(!::retrofit.isInitialized){
+             retrofit = Retrofit.Builder()
+                 .baseUrl(baseUrl)
+                 .client(httpClient.build())
+                 .addConverterFactory(GsonConverterFactory.create())
+                 .build()
+         }
+         return retrofit
+     }
+
+     fun <T> criarServico(classeServico : Class <T> ) : T{
+         return getRetrofitInstance().create(classeServico)
+     }
+
+ }
+
+}
